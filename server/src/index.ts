194cdc04ec13d23f08c211307afc85ecebaebd3c
JulianoL13 <julianov1383@gmail.com>
2024-10-29 18:00:34 -0300

    fix: alter repo structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,15 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
-// Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000; // Usa a variável de ambiente PORT, se definida
+const PORT = process.env.PORT || 8000;
 
-// Rota simples para testar o servidor
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
