74ddc067e99bbfe1330f261f4100d926be2012ea
Carlos Eduardo-PC_trabalho <eduardo201184@hotmail.com>
2024-11-01 16:17:46 -0300

    feature: login screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './pages/login';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='w-full h-screen'>
+      <Login/>
     </div>
   );
 }
