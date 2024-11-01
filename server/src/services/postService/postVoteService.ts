97f97cc4397a6ff66bff8f745a2793c58866169e
JulianoL13 <julianov1383@gmail.com>
2024-11-01 17:24:26 -0300

    fix: fix postVoteService syntax

--- server/src/services/postService/postVoteService.ts
+++ server/src/services/postService/postVoteService.ts
@@ -1,6 +1,11 @@
 import { PostVoteRepository } from "../../repositories/postRepository/postVoteRepository";
 import { PostVote } from "@prisma/client";
-import { VoteData, toggleVote } from "../../utils/toggleVote";
+
+export interface VoteData {
+  userId: number;
+  postId: number;
+  postVote: boolean;
+}
 
 export class PostVoteService {
   private postVoteRepository: PostVoteRepository;
@@ -9,15 +14,50 @@ export class PostVoteService {
     this.postVoteRepository = postVoteRepository;
   }
 
-  async toggleVoteOnPost(data: VoteData): Promise<PostVote | null> {
-    const voteData: VoteData = {
-      userId: data.userId,
-      targetId: data.postId,
-      vote: data.postVote,
-    };
+  async countVotesByPostId(postId: number) {
+    const { upvotes, downvotes } =
+      await this.postVoteRepository.countVotesByPostId(postId);
+    return { upvotes, downvotes };
+  }
 
-    // Utilize a função toggleVote
-    return await toggleVote(voteData, this.postVoteRepository);
+  private isDuplicateVote(existingVote: boolean, newVote: boolean): boolean {
+    return existingVote === newVote;
+  }
+
+  private async removeVote(userId: number, postId: number): Promise<null> {
+    await this.postVoteRepository.deleteVote(userId, postId);
+    return null;
+  }
+
+  private async updateExistingVote(
+    voteId: number,
+    newVote: boolean,
+  ): Promise<PostVote> {
+    return this.postVoteRepository.updateVote(voteId, {
+      postVote: newVote,
+    });
+  }
+
+  private async createNewVote(data: VoteData): Promise<PostVote> {
+    return this.postVoteRepository.votePost(data);
+  }
+
+  async toggleVoteOnPost(data: VoteData): Promise<PostVote | null> {
+    const { userId, postId, postVote } = data;
+
+    const existingVote = await this.postVoteRepository.getVoteByUserAndPostId(
+      postId,
+      userId,
+    );
+
+    if (existingVote) {
+      if (this.isDuplicateVote(existingVote.postVote, postVote)) {
+        return await this.removeVote(userId, postId);
+      }
+      return await this.updateExistingVote(existingVote.id, postVote);
+    } else {
+      return await this.createNewVote(data);
+    }
   }
 
   async hasUserVoted(postId: number, userId: number): Promise<boolean> {
@@ -26,11 +66,5 @@ export class PostVoteService {
       userId,
     );
     return existingVote !== null;
-  }
-
-  async countVotesByPostId(postId: number) {
-    const { upvotes, downvotes } =
-      await this.postVoteRepository.countVotesByPostId(postId);
-    return { upvotes, downvotes };
   }
 }
