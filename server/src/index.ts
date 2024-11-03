6d115794b9cbea88ea3ce4ede3d8137b748671a1
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-11-03 17:33:26 -0300

    feat: connect  authController to routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+import routes from "./routes/routes";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
